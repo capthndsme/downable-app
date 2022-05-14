b6781c819cb444931d6b419fed59260f08b93508
Carlos Daniel Chico <nieoy@msn.com>
2022-05-14 22:34:16 +0800

    oops, v2

--- test.js
+++
@@ -1,9 +0,0 @@
-const ytdl = require("ytdl-core");
-const fs = require("fs");
-ytdl(`http://www.youtube.com/watch?v=1-JCbVBRPQo`, {
-  quality: 140,
-})
-.pipe(
-    fs.createWriteStream('good.mp4')
-)
-
